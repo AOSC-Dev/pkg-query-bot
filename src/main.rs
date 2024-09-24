35b1d9221d68719b89547d9b51b6f8e62d5b28c8
eatradish <sakiiily@aosc.io>
2024-09-24 23:23:41 +0800

    feat: add /search

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::fmt::Display;
+use std::sync::Arc;
 
-use reqwest::Client;
-use serde::Deserialize;
+use package_site::PackageSiteClient;
 use teloxide::{prelude::*, types::ParseMode, utils::command::BotCommands};
+
+mod package_site;
 
 #[derive(BotCommands, Clone, Debug)]
 #[command(
@@ -13,59 +14,21 @@ enum Cmd {
     Help,
     #[command(description = "Get a package infomation (e,g: /pkg oma)")]
     Pkg(String),
-}
-
-#[derive(Debug, Deserialize)]
-struct Pkg {
-    name: String,
-    description: String,
-    version_matrix: Vec<Version>,
-}
-
-impl Display for Pkg {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "<b>{}</b>", self.name)?;
-        writeln!(f)?;
-        writeln!(f, "{}", self.description)?;
-        writeln!(f)?;
-
-        for v in &self.version_matrix {
-            for m in &v.meta {
-                if !m.hasmeta || m.version.is_empty() {
-                    continue;
-                }
-
-                writeln!(f, "<b>{}</b>: <code>{}</code>", v.repo, m.version)?;
-
-                break;
-            }
-        }
-
-        Ok(())
-    }
-}
-
-#[derive(Debug, Deserialize)]
-struct Version {
-    repo: String,
-    meta: Vec<Meta>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Meta {
-    hasmeta: bool,
-    version: String,
+    #[command(description = "Search packages (e,g: /search oma)")]
+    Search(String),
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
     let bot = Bot::from_env();
-    let client = Client::builder().user_agent("bot").build()?;
+    let client = Arc::new(PackageSiteClient::new(
+        "https://packages.aosc.io".to_string(),
+    )?);
 
     let handler =
         Update::filter_message().branch(dptree::entry().filter_command::<Cmd>().endpoint(
-            |bot: Bot, msg: Message, cmd: Cmd, client: Client| async move {
+            |bot: Bot, msg: Message, cmd: Cmd, client: Arc<PackageSiteClient>| async move {
                 answer(bot, msg, cmd, client).await
             },
         ));
@@ -81,10 +44,15 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Cmd, client: Client) -> ResponseResult<()> {
+async fn answer(
+    bot: Bot,
+    msg: Message,
+    cmd: Cmd,
+    client: Arc<PackageSiteClient>,
+) -> ResponseResult<()> {
     match cmd {
         Cmd::Pkg(arg) => {
-            let pkg = match get_pkg(&client, &arg).await {
+            let pkg = match client.get_package(&arg).await {
                 Ok(pkg) => pkg,
                 Err(e) => {
                     bot.send_message(msg.chat.id, e.to_string()).await?;
@@ -100,20 +68,20 @@ async fn answer(bot: Bot, msg: Message, cmd: Cmd, client: Client) -> ResponseRes
             bot.send_message(msg.chat.id, Cmd::descriptions().to_string())
                 .await?;
         }
+        Cmd::Search(arg) => {
+            let result = match client.search(&arg).await {
+                Ok(res) => res,
+                Err(e) => {
+                    bot.send_message(msg.chat.id, e.to_string()).await?;
+                    return Ok(());
+                }
+            };
+
+            bot.send_message(msg.chat.id, result.fmt_result(&arg))
+                .parse_mode(ParseMode::Html)
+                .await?;
+        }
     }
 
     Ok(())
-}
-
-async fn get_pkg(client: &Client, name: &str) -> anyhow::Result<Pkg> {
-    Ok(client
-        .get(format!(
-            "https://packages.aosc.io/packages/{}?type=json",
-            name
-        ))
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<Pkg>()
-        .await?)
 }
