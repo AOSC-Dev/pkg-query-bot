e955f60a666e57a8682951a31567a20d64103422
eatradish <sakiiily@aosc.io>
2025-09-12 17:35:13 +0800

    feat: add qq bot feature

---
+++ pkg-query-bot-qq/src/package_site.rs
@@ -0,0 +1,75 @@
+use std::fmt::Display;
+
+use pkgsite_lib::{info::Info, search::Search};
+
+pub struct Pkg<'a> {
+    inner: &'a Info,
+}
+
+impl<'a> From<&'a Info> for Pkg<'a> {
+    fn from(inner: &'a Info) -> Self {
+        Self { inner }
+    }
+}
+
+impl Display for Pkg<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self.inner.name)?;
+        writeln!(f)?;
+        writeln!(f, "{}", self.inner.description)?;
+        writeln!(f)?;
+
+        for v in &self.inner.version_matrix {
+            for m in &v.meta {
+                if !m.hasmeta || m.version.is_empty() {
+                    continue;
+                }
+
+                writeln!(f, "{}: {}", v.repo, m.version)?;
+
+                break;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+pub struct SearchResult<'a> {
+    inner: &'a Search,
+}
+
+impl<'a> From<&'a Search> for SearchResult<'a> {
+    fn from(inner: &'a Search) -> Self {
+        Self { inner }
+    }
+}
+
+impl SearchResult<'_> {
+    pub fn fmt_result(&self, search: &str, pacakge_site_url: &str) -> String {
+        let mut s = String::new();
+        s.push_str(&format!(
+            "Found {} matching package(s):\n\n",
+            self.inner.packages.len()
+        ));
+        for (idx, pkg) in self.inner.packages.iter().enumerate() {
+            if idx > 10 {
+                s.push('\n');
+                s.push_str(&format!(
+                    "For more results, check out {}/search?q={}&noredir=true",
+                    pacakge_site_url, search
+                ));
+                break;
+            }
+
+            s.push_str(&format!("{}/packages/{}", pacakge_site_url, pkg.name));
+            s.push('\n');
+        }
+
+        s
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.inner.packages.is_empty()
+    }
+}
---
+++ pkg-query-bot-tg/src/main.rs
@@ -0,0 +1,76 @@
+use std::{
+    sync::{Arc, OnceLock},
+};
+
+use pkgsite_lib::PackagesSiteClient;
+use teloxide::{
+    Bot,
+    dispatching::{HandlerExt, UpdateFilterExt},
+    dptree,
+    prelude::Dispatcher,
+    types::{Message, Update},
+    utils::command::BotCommands,
+};
+
+mod tg;
+mod package_site;
+
+#[derive(BotCommands, Clone, Debug)]
+#[command(
+    rename_rule = "lowercase",
+    description = "Bot supports the following commands:"
+)]
+enum Cmd {
+    Help,
+    #[command(description = "Get a package infomation (e,g: /pkg oma)")]
+    Pkg(String),
+    #[command(description = "Search packages (e,g: /search oma)")]
+    Search(String),
+}
+
+pub static PSC: OnceLock<Arc<PackagesSiteClient>> = OnceLock::new();
+
+#[inline]
+pub fn not_found_pkg(pkg: &str) -> String {
+    format!(
+        "Package <b>{}</b> not found\n\nDidn't find what you need? <a href=\"https://github.com/AOSC-Dev/aosc-os-abbs/issues/new?title=pakreq%3A%20{}&body=URL%3A%20%0A%0ADescription%3A%20\">Request for the package</a>",
+        pkg, pkg
+    )
+}
+
+#[inline]
+pub fn not_match_pkg(pkg: &str) -> String {
+    format!(
+        "No matching package for <b>{}</b>\n\nDidn't find what you need? <a href=\"https://github.com/AOSC-Dev/aosc-os-abbs/issues/new?title=pakreq%3A%20{}&body=URL%3A%20%0A%0ADescription%3A%20\">Request for the package</a>",
+        pkg, pkg
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    let client = Arc::new(PackagesSiteClient::from_env());
+
+    tg(client).await;
+}
+
+async fn tg(client: Arc<PackagesSiteClient>) {
+    let bot = Bot::from_env();
+
+    let handler =
+        Update::filter_message().branch(dptree::entry().filter_command::<Cmd>().endpoint(
+            |bot: Bot, msg: Message, cmd: Cmd, client: Arc<PackagesSiteClient>| async move {
+                tg::answer(bot, msg, cmd, client).await
+            },
+        ));
+
+    Dispatcher::builder(bot.clone(), handler)
+        // Pass the shared state to the handler as a dependency.
+        .dependencies(dptree::deps![client])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+}
